43360c4569658ca7ea18f439fb0b6906af9c5768
madushankaf <madushankafonseka@live.com>
2023-12-10 21:44:31 +0530

    change the image to return as a base64 string

--- octave_flight_modeling/rest_interface.py
+++ octave_flight_modeling/rest_interface.py
@@ -1,15 +1,21 @@
 from flask import Flask, send_file
 import base64
 import subprocess
+import base64
 
 app = Flask(__name__)
 
 @app.route('/flight-velocity', methods=['GET'])
+
 def get_flight_velocity():
     subprocess.call(['octave',  'flight_velocity_modeling.m'])
     
-    # Return the saved image file
-    return send_file('output_plot.png', mimetype='image/png')
+    # Convert the image to base64
+    with open('output_plot.png', 'rb') as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+    
+    # Return the base64 encoded image as a string
+    return encoded_string
 
 if __name__ == '__main__':
     app.run(debug=True)
